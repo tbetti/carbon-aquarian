ae4dbd18fc48465584b41c5430e59069c0ffd712
tbetti <tiana.bettinson@gmail.com>
2025-10-23 15:08:11 +0100

    changed url to localhose:8000

--- projects/carbon-aquarian-frontend/src/api/carbonApi.ts
+++ projects/carbon-aquarian-frontend/src/api/carbonApi.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || '', // set REACT_APP_API_URL in .env if you have a backend; empty uses same origin
+  baseURL: 'http://localhost:8000', // set REACT_APP_API_URL in .env if you have a backend; empty uses same origin
   headers: { 'Content-Type': 'application/json' },
 })
 
@@ -20,5 +20,6 @@ export type TripResult = {
 
 export async function submitTripApi(data: SubmitTripRequest): Promise<TripResult> {
   const res = await api.post<TripResult>('/calculate_and_reward', data)
+  // const res = { data: { carbon_saved_kg: 3.00675, carbon_points: 3, txid: 'UZV7YCOWK6NVIE7SHJTLWEKR' } }
   return res.data
 }
